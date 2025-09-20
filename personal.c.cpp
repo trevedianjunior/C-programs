91f60fc4c066135aaedf77442d224ee5a6e27bbc
trevedianjunior <trevedianjunior@gmail.com>
2025-09-20 06:41:02 +0300

    Add files via upload

---
+++ personal.c.cpp
@@ -0,0 +1,24 @@
+/*
+Name:TREVEDIAN JUNIOR
+RGE:PA106/G/28831/25
+Description:User prompt
+*/
+#include<stdio.h>
+
+//main function
+int main() {
+	
+	int height;
+	int id_number;
+	int bank_balance;
+	printf("Enter your height: ");
+	scanf("%d", &height);
+	
+	printf("Enter your id number: ");
+	scanf("%d", &id_number);
+	
+	printf("Enter your bank balance: ");
+	scanf("%d", &bank_balance);
+	
+	return 0;
+}
---
+++ units.c.cpp
@@ -0,0 +1,27 @@
+/*
+NAME:TREVEDIAN JUNIOR
+REG:PA106/G/28831/25
+Description:User prompt
+*/
+#include <stdio.h>
+
+
+
+//main function
+int main() {
+	
+	int units;
+	printf("Enter your units: ");
+	printf("SPM2101 discrete mathematics \n");
+	printf("SPM2104 mathematics for \n");
+	printf("UCU2105 fundamentals of ICT \n");
+	printf("SPI2102 computer systems and organization \n");
+	printf("UCU2101 communication skills \n");
+	printf("SPC2101 introduction to programming and prblem solving \n");
+	printf("SSE2101 fundamentals of systems analysis and design \n");
+	
+	return 0;
+}
+	
+	
+	
