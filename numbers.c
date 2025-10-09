cb4ffac8661c1fb2b3f48f9e935271b7423c9df7
trevedianjunior <trevedianjunior@gmail.com>
2025-10-09 11:22:10 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,20 @@
+/*
+Name: Trevedian Junior
+Reg No: PA106/G/28831/25
+Date: 6th October 2025
+Description: A program to print all numbers from 100 down to 50
+in descending order 
+*/
+
+#include <stdio.h>
+
+int main () {
+
+	int i;
+	for(i=100; i>=50; i--) {
+	    printf("%d \n", i);
+	}
+	
+	return 0;
+		
+}
